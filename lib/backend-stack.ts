5a8ee72711d9576d793eff75e515b3c9c4699009
Theophilus Asante Frimpong <theophilusfrimpong17@gmail.com>
2026-07-30 07:31:21 +0000

    Add getVendors Lambda function and integrate with API Gateway for vendor management

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class BackendStack extends cdk.Stack{
   constructor(scope: Construct, id: string, props?:cdk.StackProps) {
@@ -14,6 +16,52 @@ export class BackendStack extends cdk.Stack{
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY // for development only
+    })
+
+    // Lambda Functions
+    const lambdaEnv = {TABLE_NAME: vendorTable.tableName};
+
+    const createVendorLambda = new NodejsFunction(this, 'CreateVendorHandler', {
+      entry: 'lambda/createVendor.ts',
+      handler: 'handler',
+      environment: lambdaEnv
+    })
+
+    const getVendorsLambda = new NodejsFunction(this, 'GetVendorHandler', {
+      entry: 'lambda/getVendors.ts',
+      handler: 'handler',
+      environment: lambdaEnv
+    })
+
+    const deleteVendorsLambda = new NodejsFunction(this, 'DeleteVendorHandler', {
+      entry: 'lambda/deleteVendor.ts',
+      handler: 'handler',
+      environment: lambdaEnv
+    })
+
+    // Permissions (Least Privilege)
+    vendorTable.grantWriteData(createVendorLambda);
+    vendorTable.grantReadData(getVendorsLambda);
+    vendorTable.grantWriteData(deleteVendorsLambda)
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'VendorApi', {
+      restApiName: 'Vendor Service',
+      defaultCorsPreflightOptions:{
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: ['Content-Type', 'Authorization']
+      }
+    });
+
+    const vendors = api.root.addResource('vendors');
+    vendors.addMethod('POST', new apigateway.LambdaIntegration(createVendorLambda));
+    vendors.addMethod('GET', new apigateway.LambdaIntegration(getVendorsLambda));
+    vendors.addMethod('DELETE', new apigateway.LambdaIntegration(deleteVendorsLambda))
+
+    // Outputs
+    new cdk.CfnOutput(this, 'ApiEndpoint', {
+      value: api.url
     })
   }
 }
